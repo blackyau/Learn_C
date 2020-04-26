c633de5fee4df4af9fb83ff960ff2d4d2e3b6711
blackyau <blackyau426@gmail.com>
2020-04-26 23:38:01 +0800

    Update 查找二叉树 未完成删除

--- tree_search.c
+++ tree_search.c
@@ -8,19 +8,33 @@ typedef struct node {
 	struct node* right;
 }node;
 
-static struct node head_data = {NULL, 3, NULL}; // 初始化 head
+static struct node head_data = {NULL, 33, NULL}; // 初始化 head
 static node* head = &head_data; // 初始化 head 指针
 
 void insert(int data); // 插入数据
 node* find(int data); // 查找值,返回节点
+void delete(int data); // 删除数据
+void preOrder(node* root); // 前序遍历
+void inOrder(node* root); // 中序遍历
+void pastOrder(node* root); // 后序遍历
+void show(node* root);
 
 void main() {
-	insert(0);
-	insert(1);
-	insert(4);
-	insert(6);
-	printf("%d\n", find(4)->value);
-	printf("%d\n", find(10));
+	int data_list[15] = {55, 50, 16, 17, 19, 15, 58, 66, 51, 13, 34, 18, 51, 25, 27};
+	for (int i = 0; i < 15; i++) insert(data_list[i]);
+	printf("%d\n", find(66)->value);
+	printf("%d\n\n", find(10));
+	printf("original:\n");
+	show(head);
+	printf("del 55 after:\n");
+	delete(55);
+	show(head);
+	printf("del 13 after:\n");
+	delete(13);
+	show(head);
+	printf("del 18 after:\n");
+	delete(18);
+	show(head);
 }
 
 void insert(int data) { // 插入数据
@@ -51,7 +65,7 @@ void insert(int data) { // 插入数据
 	}
 }
 
-node* find(int data) {
+node* find(int data) { // 查找节点
 	node* temp = head;
 	while (temp != NULL) { // 主循环
 		if (data < temp->value) temp = temp->left; // 查找数据比当前节点小，继续在左子树找
@@ -61,4 +75,74 @@ node* find(int data) {
 	/* 二叉树中的每一个节点值，都比它小或大，就是没有一个和它相等的
 	退出循环的时候 temp == NULL */
 	return NULL; 
+}
+
+void delete(int data) { // 删除节点
+	node* rm = NULL;
+	node* rm_father = head;
+	// 主循环，找到值匹配的节点
+	while (rm != NULL && rm->value != data) {
+		rm_father = rm; // 储存父节点的信息,后续的删除操作需要用到
+		if (data > rm->value)rm = rm->right; // 删除值大于当前节点值，说明值在右子树
+		else rm = rm->left; // 删除之小于当前值，说明值在左子树
+	}
+	if (rm == NULL)return; // 在树中没有找到要删除的值
+	
+ /* 要删除节点有两个子节点
+	要保证查找树结构正确，就要拿 删除节点右子树中 最小的节点 替换自己
+	因为节点要比左边的大，右边的小 */
+	if (rm->left != NULL && rm->right != NULL) { // 查找右子树中最小节点
+		node* min = rm->right; // 进入右子树
+		node* min_father = rm; // 最小节点的父节点
+		while (min->left != NULL) {
+			min_father = min; // 记录最小节点的父节点信息
+			min = min->left; // 最小的肯定在左边（查找树特性）
+		}
+		rm->value = min->value; // 将右子树中的最小节点数值，替换到删除节点
+		min_father->left = NULL; // 将最小节点移除树
+		free(min); // 删除（rm的值已经被min替换，所以rm位置不变无需删除，反而min节点没用了）
+		return;
+	}
+
+	// 删除节点是叶子节点（没有子节点）或仅有一个子节点
+	node* child; // rm 的子节点
+	if (rm->left != NULL)child = rm->left; // 删除节点有左子树
+	else if (rm->right != NULL)child = rm->right; // 删除节点有右子树
+	else child = NULL; // 删除节点没有子节点（叶子节点）
+
+	if (rm_father == NULL) head = child; // 删除节点没有父节点，删除的是根节点
+	else if (rm_father->left == rm)rm_father->left = child; // 使用删除节点的左节点替换自己
+	else rm_father->right = child; // 使用删除节点的右节点替换自己
+	free(rm);
+}
+
+void preOrder(node* root) { // 前序遍历
+	if (root == NULL)return;
+	printf("%d ", root->value); // 先打印自己
+	preOrder(root->left); // 再打印左子树
+	preOrder(root->right); // 最后打印右子树
+}
+
+void inOrder(node* root) { // 中序遍历
+	if (root == NULL)return;
+	inOrder(root->left); // 先打印左子树
+	printf("%d ", root->value); // 后打印自己
+	inOrder(root->right); // 最后打印右子树
+}
+
+void pastOrder(node* root) {
+	if (root == NULL)return;
+	pastOrder(root->left); // 先打印左子树
+	pastOrder(root->right); // 再打印右子树
+	printf("%d ", root->value); // 最后打印自己
+}
+
+void show(node* root) {
+	printf("preOrder :");
+	preOrder(root);
+	printf("\ninOrder  :");
+	inOrder(root);
+	printf("\npastOrder:");
+	pastOrder(root);
+	printf("\n\n");
 }
