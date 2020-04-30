857bc08d24cafcf0fefa05402a78e0b3ab9aa5b4
blackyau <blackyau426@gmail.com>
2020-04-30 23:42:32 +0800

    UPdate Huffman 树 完成单个节点初始化

--- tree_huffman.c
+++ tree_huffman.c
@@ -1,6 +1,7 @@
 // Huffman 树
 #include<stdio.h>
 #include<stdlib.h>
+#define SIZE 6
 
 typedef struct node {
 	char key; // 字符
@@ -20,5 +21,29 @@ void showTree(node* head);
 void showCode(node* head);
 
 void main() {
+	node* node_list[SIZE]; // 节点指针
+	char key_list[SIZE] = {'F', 'O', 'R', 'G', 'E', 'T'}; // 字符
+	int wight_list[SIZE] = { 2, 3, 4, 4, 5, 7 }; // 权重
+	for (int i = 0; i < SIZE; i++) {
+		node* temp = (node*)malloc(sizeof(node));
+		node_list[i] = temp;
+	}
+	// 创建 Huffman 树，并获取它的头节点
+	node* head = create(node_list, key_list, wight_list, SIZE); 
+}
+
+node* create(node* node_list[], int key_list[], int weight_list[], int list_len) {
+	// 初始化所有节点的信息
+	for (int i = 0; i < list_len; i++) {
+		node* temp = node_list[i];
+		temp->key = key_list[i]; // 字符
+		temp->weight = weight_list[i]; // 权重
+		temp->father = NULL; // 父节点
+		temp->left = NULL; // 左子树
+		temp->right = NULL; // 右子树
+	}
+}
+
+void select(node* node_list[], int list_len, int* min1, int* main2) {
 
 }
